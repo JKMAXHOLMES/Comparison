2b3fc4020e35b2187255fb0cb0c93a031f83d250
Nidhi <Nidhi@VICTORY>
2022-02-01 12:19:10 +0530

    Add project files.

---
+++ Comparison/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Comparison.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
